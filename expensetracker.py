02fc1e2ac3e7233c1a14e449a81bf8219ff77016
notirus-1 <vfjz2s7s9w@privaterelay.appleid.com>
2026-09-05 20:11:53 +0300

    optimized code & fixed several bugs

--- expensetracker.py
+++ expensetracker.py
@@ -8,52 +8,50 @@ file_path = base_dir / "data.json"
 if file_path.exists():
     utilities_dict = json.loads(file_path.read_text(encoding="utf-8"))
 else:
-    utilities_dict = {"electricity": "0", "water": "0", "gas": "0"}
+    utilities_dict = {"electricity": 0, "water": 0, "gas": 0}
     file_path.write_text(json.dumps(utilities_dict), encoding="utf-8")
 # print out greeting message
-print("_____                                  _____               _             ")
-print("| ____|_  ___ __   ___ _ __  ___  ___  |_   _| __ __ _  ___| | _____ _ __ ")
-print("|  _| \ \/ / '_ \ / _ \ '_ \/ __|/ _ \   | || '__/ _` |/ __| |/ / _ \ '__|")
-print("| |___ >  <| |_) |  __/ | | \__ \  __/   | || | | (_| | (__|   <  __/ | ")
-print("|_____/_/\_\ .__/ \___|_| |_|___/\___|   |_||_|  \__,_|\___|_|\_\___|_|   ")
-print("            |_|                                                            ")
-
-print("\n")
+art = """
+░█▀▀░█░█░█▀█░█▀▀░█▀█░█▀▀░█▀▀░░░▀█▀░█▀▄░█▀█░█▀▀░█░█░█▀▀░█▀▄
+░█▀▀░▄▀▄░█▀▀░█▀▀░█░█░▀▀█░█▀▀░░░░█░░█▀▄░█▀█░█░░░█▀▄░█▀▀░█▀▄
+░▀▀▀░▀░▀░▀░░░▀▀▀░▀░▀░▀▀▀░▀▀▀░░░░▀░░▀░▀░▀░▀░▀▀▀░▀░▀░▀▀▀░▀░▀
+"""
+print(art)
 # initial user interface
 while True:
-    answer = input("Welcome to the expense tracker. Would you like to view or edit your expenses if so, press 1 to view, 2 to edit and q to exit. \n")
-    if answer == "1":
+    choice = input("\nWelcome to the expense tracker. Would you like to view or edit your expenses if so, press 1 to view, 2 to edit, 3 to see total and q to exit. \n")
+    if choice == '1':
         print(f'Your electricity expenses: {utilities_dict.get('electricity')}')
         print(f'Your water expenses: {utilities_dict.get('water')}')
         print(f'Your gas expenses: {utilities_dict.get('gas')}')
-    elif answer == "2":
+    elif choice == '2':
         edit = input('Which expense would you like to edit? e for electricity, w for water, g for gas r to return\n')
-        
         if edit == 'e':
             utilities_dict['electricity'] = input('enter your electricity expense: ')
             print('expense set! ')
         elif edit == 'w':
             utilities_dict['water'] = input('enter your water expense: ')
-            print('expense set! ')
+            print('expense set! ')       
         elif edit == 'g':
             utilities_dict['gas'] = input('enter your gas expense: ')
             print('expense set! ')
         elif edit == 'r':
-            sleep(1)
+            continue
         else:
             print('invalid response.\n')
             continue
-
-    elif answer == 'q':
-        print('bye!')
+    elif choice == '3':
+        print('Total of your expenses are: ', utilities_dict.get('electricity') + utilities_dict.get('water') + utilities_dict.get('gas'))
+    elif choice == 'q':
+        print('bye')
         with open('data.json', 'w') as f:
             file_path.write_text(json.dumps(utilities_dict), encoding="utf-8")
         break
     else:
-        print('Invalid choice try again')
+        print('invalid response.\n')
         continue
-    return_input = input('Would you like to make any more changes? (y to return, n to quit):\n').strip().lower()
+    return_input = input('Would you like to make any more changes? (y to return, n to quit): ').strip().lower()
     if return_input == 'n':
-        print('Bye!')
+        print('Bye! \n')
         file_path.write_text(json.dumps(utilities_dict), encoding="utf-8")
         break
